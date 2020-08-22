af20ecb48c401a306c3d89dcf011948c79a331e9
yamasan <general@malrone.info>
2020-08-22 10:59:17 +0900

    プロジェクト ファイルを追加します。

---
+++ Mojibake/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Mojibake
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.label3 = new System.Windows.Forms.Label();
+			this.cbOut = new System.Windows.Forms.ComboBox();
+			this.cbIn = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Location = new System.Drawing.Point(84, 3);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(713, 200);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.label3);
+			this.splitContainer1.Panel1.Controls.Add(this.cbOut);
+			this.splitContainer1.Panel1.Controls.Add(this.cbIn);
+			this.splitContainer1.Panel1.Controls.Add(this.label1);
+			this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.label2);
+			this.splitContainer1.Panel2.Controls.Add(this.textBox2);
+			this.splitContainer1.Size = new System.Drawing.Size(800, 464);
+			this.splitContainer1.SplitterDistance = 232;
+			this.splitContainer1.TabIndex = 1;
+			// 
+			// label3
+			// 
+			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(297, 212);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(17, 12);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "→";
+			// 
+			// cbOut
+			// 
+			this.cbOut.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.cbOut.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbOut.FormattingEnabled = true;
+			this.cbOut.Location = new System.Drawing.Point(320, 209);
+			this.cbOut.Name = "cbOut";
+			this.cbOut.Size = new System.Drawing.Size(205, 20);
+			this.cbOut.TabIndex = 3;
+			this.cbOut.SelectedIndexChanged += new System.EventHandler(this.cbOut_SelectedIndexChanged);
+			// 
+			// cbIn
+			// 
+			this.cbIn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.cbIn.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbIn.FormattingEnabled = true;
+			this.cbIn.Location = new System.Drawing.Point(86, 209);
+			this.cbIn.Name = "cbIn";
+			this.cbIn.Size = new System.Drawing.Size(205, 20);
+			this.cbIn.TabIndex = 2;
+			this.cbIn.SelectedIndexChanged += new System.EventHandler(this.cbIn_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 3);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(75, 12);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "文字化け状態";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(3, 6);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(41, 12);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "修正後";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox2.Location = new System.Drawing.Point(84, 3);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(713, 222);
+			this.textBox2.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 464);
+			this.Controls.Add(this.splitContainer1);
+			this.Name = "Form1";
+			this.Text = "文字化け修正";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel1.PerformLayout();
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.ComboBox cbOut;
+		private System.Windows.Forms.ComboBox cbIn;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
---
+++ Mojibake/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Mojibake
+{
+	public partial class Form1 : Form
+	{
+		private bool initEnd = false;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			List<ComboEncode> encList = new List<ComboEncode>();
+
+			//utf8 65001 
+			//sjis 932
+
+			foreach( EncodingInfo ei in Encoding.GetEncodings() )
+			{
+				encList.Add( new ComboEncode(ei) );
+			}
+
+			cbIn.Items.AddRange(encList.ToArray());
+			cbOut.Items.AddRange(encList.ToArray());
+
+			for( int i = 0; i < encList.Count; i++ )
+			{
+				if( encList[i].CodePage == 932 )
+				{
+					cbIn.SelectedIndex = i;
+				}
+				else if( encList[i].CodePage == 65001 )
+				{
+					cbOut.SelectedIndex = i;
+				}
+			}
+			
+			initEnd = true;
+			OutputText();
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			OutputText();
+		}
+
+		private void cbIn_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			OutputText();
+		}
+
+		private void cbOut_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			OutputText();
+		}
+
+		private void OutputText()
+		{
+			if( !initEnd ) return;
+
+			string src = textBox1.Text;
+			Encoding utf8 = ((ComboEncode)cbOut.SelectedItem).Encoding;
+			Encoding sjis = ((ComboEncode)cbIn.SelectedItem).Encoding;
+			byte[] utf8byte = sjis.GetBytes(src);
+			textBox2.Text = utf8.GetString(utf8byte);
+		}
+
+	}
+
+	public class ComboEncode
+	{
+		public int CodePage 
+		{
+			get
+			{
+				return encInfo.CodePage;
+			}
+		}
+
+		public Encoding Encoding
+		{
+			get
+			{
+				return encInfo.GetEncoding();
+			}
+		}
+
+		private EncodingInfo encInfo;
+
+		public ComboEncode( EncodingInfo ei )
+		{
+			encInfo = ei;
+		}
+
+		public override string ToString()
+		{
+			return $"{encInfo.DisplayName}[{encInfo.CodePage}]";
+		}
+	}
+}
